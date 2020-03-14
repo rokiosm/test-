2541bd1d7c670e79a548a348c06f9e602758d423
rokiosm <47125027+rokiosm@users.noreply.github.com>
2020-03-14 12:04:09 +0900

    3/14

--- copang/01_copang.py
+++ copang/01_copang.py
@@ -2,12 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 
 headers = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36"
 }
 
-# 스킨케어 - 쿠팡랭킹순 모든 상품,가격 가져오기
+
 for idx in range(1, 17):
-    url = "http://www.coupang.com/np/categories/176860?page=" + str(idx)
+    url = "https://www.coupang.com/np/categories/176530=" + str(idx)
 
     print(url)
     result = requests.get(url, headers=headers)
@@ -15,6 +15,7 @@ for idx in range(1, 17):
 
     div = soup_obj.findAll("div", {"class": "name"})
     lis = soup_obj.find("ul", {"id": "productList"}).findAll("li")
+
 
     for li in lis:
         product = li.find("div", {"class": "name"})
